dfc526b97e2e9e85f71889ac7303372ca231b812
Toni B <anthonyprincedom@hotmail.com>
2021-03-15 23:13:42 +0100

    - Added material module - Added toolbar component

---
+++ src/app/common/toolbar/app-toolbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+/**
+ * @title Basic toolbar
+ */
+@Component({
+  selector: 'app-toolbar',
+  templateUrl: 'app-toolbar.component.html',
+  styleUrls: ['app-toolbar.component.sass'],
+})
+export class AppToolbar {}
